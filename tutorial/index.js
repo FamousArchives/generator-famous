2aabd0be6f1d772f229a655b87caef3b946bbad9
Myles Borins <myles@famo.us>
2014-04-16 14:26:09 -0700

    Error Checking

--- tutorial/index.js
+++ tutorial/index.js
@@ -1,16 +1,22 @@
 'use strict';
 // var util = require('util');
 var yeoman = require('yeoman-generator');
-
+var chalk = require('chalk');
 
 var TutorialGenerator = yeoman.generators.NamedBase.extend({
   init: function () {
     console.log('Setting up the ' + this.name + ' tutorial.');
+    this.path = 'https://github.com/Famous/tutorial-' + this.name + '/archive/master.zip';
   },
 
   download: function () {
-    this.extract('https://github.com/Famous/tutorial-' + this.name + '/archive/master.zip', '', function () {
-      this.installDependencies();
+    this.extract(this.path, process.cwd(), function (err) {
+      if (err) {
+        console.log(chalk.red('ERROR: ') + 'Ruhroh, the tutorial ' + this.name + ' does not seem to exist... please try again later');
+      }
+      else {
+        this.installDependencies();
+      }
     }.bind(this));
   }
 });
