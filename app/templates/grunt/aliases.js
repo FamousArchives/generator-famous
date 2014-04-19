a103348ba8e93f5c2d90421709ec5b55ab4dc16f
Myles Borins <myles@famo.us>
2014-04-18 18:47:27 -0700

    Update templating file

--- app/templates/grunt/aliases.js
+++ app/templates/grunt/aliases.js
@@ -1,6 +1,13 @@
+var metrics = require('famous-metrics');
+
 module.exports = function (grunt) {
   'use strict';
   grunt.registerTask('serve', function (target) {
+    
+    if (!metrics.getTinfoil()) {
+      metrics.track('grunt serve', {});
+    }
+    
     if (target === 'dist') {
       return grunt.task.run(['build', 'connect:dist:keepalive']);
     }
