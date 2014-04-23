067084dede6a143fc6f98a00815450e985ccb4ab
Myles Borins <myles@famo.us>
2014-04-23 01:09:44 -0700

    Update tests

--- test/test-load.js
+++ test/test-load.js
@@ -5,6 +5,10 @@ var assert = require('assert');
 describe('famous generator', function () {
   it('can be imported without blowing up', function () {
     var app = require('../app');
+    var view = require('../view');
+    var opinionated = require('../opinionated');
     assert(app !== undefined);
+    assert(view !== undefined);
+    assert(opinionated !== undefined);
   });
 });
