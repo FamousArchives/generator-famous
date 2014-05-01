17c735b882f16f04a0470c11990cc321a9a3a21a
Myles Borins <myles@famo.us>
2014-05-02 01:51:06 +0200

    Added animation to basic scaffold

--- app/templates/src/_main.js
+++ app/templates/src/_main.js
@@ -1,10 +1,11 @@
-/*globals define*/
+/* globals define */
 define(function(require, exports, module) {
     'use strict';
     // import dependencies
     var Engine = require('famous/core/Engine');
+    var Modifier = require('famous/core/Modifier');
+    var Transform = require('famous/core/Transform');
     var ImageSurface = require('famous/surfaces/ImageSurface');
-    var StateModifier = require('famous/modifiers/StateModifier');
 
     // create the main context
     var mainContext = Engine.createContext();
@@ -12,12 +13,17 @@ define(function(require, exports, module) {
     // your app here
     var logo = new ImageSurface({
         size: [200, 200],
-        content: '/content/images/famous_logo.png'
+        content: '/content/images/famous_logo.png',
+        classes: ['backfaceVisibility']
     });
 
-    var logoModifier = new StateModifier({
-        origin: [0.5, 0.5]
+    var initialTime = Date.now();
+    var centerSpinModifier = new Modifier({
+        origin: [0.5, 0.5],
+        transform : function() {
+            return Transform.rotateY(.002 * (Date.now() - initialTime));
+        }
     });
 
-    mainContext.add(logoModifier).add(logo);
+    mainContext.add(centerSpinModifier).add(logo);
 });
